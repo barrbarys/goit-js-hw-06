e6886b67ef9ac14a2bd44bea86fb97cd0d05994b
barrbarys <pysarchuk.ihor@gmail.com>
2022-11-08 23:05:24 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", checkLogin);
 
+function checkLogin(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Потрібно заповнити всі поля");
+  }
+  const userData = { email: email.value, Password: password.value };
+
+  console.log("Email:", userData.email);
+  console.log("Password:", userData.Password);
+  event.target.reset();
+}
