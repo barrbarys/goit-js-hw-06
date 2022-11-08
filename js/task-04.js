7e2be0b05a11fc67064280bd7ceb0765f0d2edf3
barrbarys <pysarchuk.ihor@gmail.com>
2022-11-08 21:26:32 +0200

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,12 @@
-co;
+let counterValue = 0;
+const dispCount = document.querySelector("#value");
+const minus = document.querySelector('button[data-action="decrement"]');
+const plus = document.querySelector('button[data-action="increment"]');
+minus.addEventListener("click", () => {
+  counterValue--;
+  dispCount.textContent = counterValue;
+});
+plus.addEventListener("click", () => {
+  counterValue++;
+  dispCount.textContent = counterValue;
+});
